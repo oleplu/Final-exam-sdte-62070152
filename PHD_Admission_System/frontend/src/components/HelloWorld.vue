97fc5694fdea774402da27e830e5d39c8e814e6f
Plurein <purindroper@gmail.com>
2022-05-10 17:51:24 +0700

    bad mood

--- PHD_Admission_System/frontend/src/components/HelloWorld.vue
+++ PHD_Admission_System/frontend/src/components/HelloWorld.vue
@@ -1,38 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
-  <div class="grid grid-cols-4">
-    <div>A</div>
-    <div>B</div>
-    <p>C</p>
-    <p>D</p>
-  </div>
+    <div class="nav">
+      <div class="">
+
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.nav {
+  width:auto;
+  height: 50px;
+  background-color: white;
+  margin-top:0%;
 }
 </style>
+
--- PHD_Admission_System/frontend/tailwind.config.js
+++ PHD_Admission_System/frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js,vue}",
-    "./public/index.html"],
+    "./public/index.html",],
   theme: {
     extend: {},
   },
